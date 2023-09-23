4db34729482a7425f9d8043486f1ee6f858668dd
flaging <liyupeng341@163.com>
2023-09-23 19:36:16 +0800

    daily update

---
+++ download_weibo.py
@@ -0,0 +1,88 @@
+import re
+import os
+
+# text = "[Example](https://www.example.com):This is an example link"
+
+def parse_pattern(text):
+  pattern = r'\[([^]]+)\]\(([^)]+)\):(.*)'
+  pattern2 = r'\[([^]]+)\]\(([^)]+)\)'
+
+  match = re.search(pattern, text)
+  match2 = re.search(pattern2, text)
+
+  if match:
+    title = match.group(1)
+    link = match.group(2)
+    note = match.group(3).strip()
+  elif match2:
+    title = match2.group(1)
+    link = match2.group(2)
+    note = None
+  else:
+    title = None
+    link = None
+    note = None
+  return link, (title, note)
+total_link = {}
+def match_link(file_name, total_link):
+  line_num = 0
+  for line in open(file_name).readlines():
+    line_num += 1
+    if len(line) <= 1:continue
+    link, other = parse_pattern(line)
+    # print(link, other)
+    other = other + (file_name,)
+    other = other + (line_num,)
+    if link is not None:
+      if link not in total_link.keys():
+        total_link[link] = [other]
+      else:
+        total_link[link].append(other)
+  return total_link
+import random
+import time
+def print_github(succeed, content):
+  pattern = r'github\.com/[A-Za-z0-9./\-\_]+'  
+  links = re.findall(pattern, content)
+  for link in links:
+    succeed.writelines('\n\nGithub: ['+link+']('+link+')')
+def print_md(succeed, title, link, content, links, mobile_pic_link):
+  if len(content) > 1:
+    if content.startswith(':'): content = content[1:]
+  title_content = content[:50] if len(content) > 50 else content
+  title = title if title.startswith('@') else '@'+title
+  succeed.writelines('\n\n#### [' + title_content + ' ' + title + ']('+link+')')
+  content.strip()
+  if len(content) > 1:
+    succeed.writelines('\n\nNote: '+content)
+  for num, link in enumerate(links):
+    succeed.writelines('\n\nPicture: ['+link.split('/')[-1]+']('+mobile_pic_link[0]+')')
+  print_github(succeed, content)
+  succeed.flush()
+  
+def random_sleep():
+  index = random.randint(1,10)
+  scale = 1
+  if index > 8: scale = 10
+  time.sleep(random.randint(1, 200)/200.0*scale)
+total_link = match_link('src/23-09-23-1.md', total_link)
+# total_link = open('2023-09-23-16:58:35.failed.txt', 'r').readlines()
+from  weibo import get_weibo_data_from_url as downloader
+now = time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime())
+failed = open(now+'.failed.txt', 'w')
+failed.writelines('failed urls:\n')
+succeed = open(now+'.md', 'w')
+for link in total_link.keys():
+# for link in total_link:
+  if 'weibo.com' in link:
+    link_mobile = link.replace('weibo.com', 'weibo.cn')
+    html_bytes = ''
+    try:
+      content, images, title, mobile_pic_link, html_bytes = downloader(link_mobile)
+      print_md(succeed, title, link, content, images, mobile_pic_link)
+    except:
+      failed.writelines(link+'\n')
+      failed.flush()
+      if len(html_bytes)>1:
+        print(html_bytes)
+  random_sleep()
---
+++ weibo.py
@@ -0,0 +1,45 @@
+import requests
+from lxml import html
+
+def get_weibo_data_from_url(url):
+  # url='https://weibo.cn/1402400261/LBuKWw2ii'
+  user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'
+  def get_cookie():
+    file = open('cookie.txt', 'r')
+    return file.read()
+  
+  cookie = get_cookie()
+  # 请求页面
+  print(cookie)
+  headers = {'User_Agent': user_agent, 'Cookie': cookie}
+  resp = requests.get(url, headers=headers)
+  # resp = requests.get(url)
+  resp.encoding = 'utf-8'
+  page = resp.text
+  # print(page)
+  html_bytes = page.encode('utf-8')
+  # print(html_bytes)
+  root = html.fromstring(html_bytes)
+
+  # 提取正文 
+  article = root.xpath("//span[@class='ctt']/text()")
+  content = ''.join(article)
+
+  # 提取图片链接
+  images = root.xpath('//img/@src')
+  real_images = []
+  for image in images:
+    if 'jpg' in image:
+      real_images.append(image.replace('wap180', 'large'))
+  links = root.xpath('//a/@href')
+  # print(links)
+  # print(real_images)
+  mobile_pic_link = []
+  for link in links:
+    if link.startswith('/') and '/pic/' in link:
+      mobile_pic_link.append('https://weibo.cn/'+link)
+  #     print('https://weibo.cn/'+link)
+  # print(mobile_pic_link)
+  title = root.xpath("//a[1]/text()")[3]
+  return content, real_images, title, mobile_pic_link, html_bytes
+
